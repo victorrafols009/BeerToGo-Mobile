2b7ef2848abd9858231405e640726dcde858649f
Victor Rafols <victor.rafols009@gmail.com>
2017-01-24 18:30:20 +0800

    done approval of request to drink functionality

--- app/src/main/java/com/app/drinktogo/fragments/NotificationFragment.java
+++ app/src/main/java/com/app/drinktogo/fragments/NotificationFragment.java
@@ -1,13 +1,16 @@
 package com.app.drinktogo.fragments;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.ListFragment;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.app.drinktogo.Adapter.NotificationAdapter;
 import com.app.drinktogo.Entity.Notification;
@@ -15,6 +18,7 @@ import com.app.drinktogo.R;
 import com.app.drinktogo.helper.Ajax;
 import com.app.drinktogo.helper.AppConfig;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -41,6 +45,72 @@ public class NotificationFragment extends ListFragment {
         user_id = getArguments().getInt("user_id");
 
         return view;
+    }
+
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        NotificationAdapter.ViewHolder view = (NotificationAdapter.ViewHolder) v.getTag();
+        final Notification n = view.notification;
+
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        final ProgressDialog progress = new ProgressDialog(getActivity());
+                        progress.setMessage("Logging in...");
+                        progress.setIndeterminate(false);
+                        progress.setCancelable(false);
+
+                        RequestParams data = new RequestParams();
+                        data.add("id", Integer.toString(n.id));
+
+                        Ajax.post("transaction/confirm/user", data, new JsonHttpResponseHandler(){
+                            @Override
+                            public void onStart() {
+                                progress.show();
+                            }
+
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                if (statusCode == 200) {
+                                    if(response.length() > 0){
+                                        AppConfig.showDialog(getActivity(), "Message", "Successfully allowed to drink!");
+                                    }else{
+                                        AppConfig.showDialog(getActivity(), "Message", "Approval not send. Sorry. :(");
+                                    }
+                                } else {
+                                    AppConfig.showDialog(getActivity(), "Message", "There is problem in your request. Please try again.");
+                                }
+                                Log.d("Result", response.toString());
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                                Log.d("Failed: ", ""+statusCode);
+                                Log.d("Error : ", "" + throwable);
+                                AppConfig.showDialog(getActivity(), "Message", "There is problem in your request. Please try again.");
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                progress.dismiss();
+                            }
+                        });
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Approval Confirmation")
+                .setMessage("Allow this?")
+                .setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener)
+                .show();
     }
 
     @Override
